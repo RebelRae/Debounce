6a833dc0c5b74854e2d2c4e64d543014eeb56821
Rebel Rae <Rebel@TheDeviantRebel.com>
2021-06-19 20:28:20 -0700

    initial

---
+++ README.md
@@ -0,0 +1,51 @@
+# Debounce
+
+Debounce is an *easy-to-use, adjustable* accessibility extension for users with hand tremors.
+
+Simple in principle, simple in implementation. **Let's make VSCode more accessible!**
+
+Please, feel free to request features by emailing me at [rebel@TheDeviantRebel.com](mailto:rebel@TheDeviantRebel.com)
+
+## Features
+
+> Tip: All features are available in commands.
+
+![Set Delay in Millis](images/commands.png)
+
+To set a new delay value, run the '*Set Delay in Millis*' command.
+
+![Set Delay in Millis](images/adjustment.png)
+
+To display the current becouncing delay value, run the '*Display Delay in Millis*' command.
+
+![Display Delay in Millis](images/display.png)
+
+To display the number of error corrections made since you last switched editor views, and session lifetime totals, run the '*Display Number of Errors Corrected*' command.
+
+![Set Delay in Millis](images/corrections.png)
+
+## Requirements
+
+Debounce runs as-is with no dependencies.
+
+## Extension Settings
+
+Settings are only adjusted via commands.
+
+## Known Issues
+
+Debounce has no known issues as of yet.
+
+## Release Notes
+
+Initial release. Until now I have only used it for myself. Please, feel free to request features by emailing me at [rebel@TheDeviantRebel.com](mailto:rebel@TheDeviantRebel.com)
+
+### 1.0.0
+
+Initial release.
+
+-----------------------------------------------------------------------------------------------------------
+
+Would you like to make a **donation**? lol no thanks.
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,89 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+function activate(context) {
+    let EDITOR = vscode.window.activeTextEditor
+    let DEBOUNCE_DELAY = 200
+    let PREVOIUS_MILLIS = Date.now()
+    let PREVOIUS_KEY = false
+    let ERRORS_FIXED = 0
+    let SESSION_ERRORS = 0
+    let DISPLAY_FEEDBACK = true
+
+    // Prompts user for input to set millis value
+    let setMillis = vscode.commands.registerCommand('debounce.setMillis', () => {
+        if (!DISPLAY_FEEDBACK) return
+        vscode.window.showInputBox().then(input => {
+            const previousValue = DEBOUNCE_DELAY
+            DEBOUNCE_DELAY = input
+            vscode.window.showInformationMessage((previousValue == DEBOUNCE_DELAY) ?
+                `Debounce delay is still set to ${DEBOUNCE_DELAY} millis.` :
+                `Debounce delay adjusted from ${previousValue} to ${DEBOUNCE_DELAY} millis.`)
+        })
+    })
+
+    // Displays current debounce value in millis
+    let displayMillis = vscode.commands.registerCommand('debounce.displayMillis', () => {
+        if (!DISPLAY_FEEDBACK) return
+        vscode.window.showInformationMessage(`Current debounce set to ${DEBOUNCE_DELAY} millis.`)
+    })
+
+    // Displays numbers of errors
+    let displayErrors = vscode.commands.registerCommand('debounce.displayErrors', () => {
+        if (!DISPLAY_FEEDBACK) return
+        vscode.window.showInformationMessage(`Debounce has made ${ERRORS_FIXED} corrections since editor view changed.\nDebounce has made ${SESSION_ERRORS} corrections since VSCode started.`)
+    })
+
+    // Enables information messages
+    let enableFeedback = vscode.commands.registerCommand('debounce.enableFeedback', () => {
+        DISPLAY_FEEDBACK = true
+        vscode.window.showInformationMessage(`Feedback enabled.`)
+    })
+
+    // Disables information messages
+    let disableFeedback = vscode.commands.registerCommand('debounce.disableFeedback', () => {
+        DISPLAY_FEEDBACK = false
+        vscode.window.showInformationMessage(`Feedback disabled.`)
+    })
+
+    context.subscriptions.push(setMillis)
+    context.subscriptions.push(displayMillis)
+    context.subscriptions.push(displayErrors)
+    context.subscriptions.push(enableFeedback)
+    context.subscriptions.push(disableFeedback)
+
+    vscode.window.onDidChangeActiveTextEditor(() => {
+        EDITOR = vscode.window.activeTextEditor
+        vscode.commands.executeCommand('debounce.displayErrors')
+        ERRORS_FIXED = 0
+    })
+
+    vscode.workspace.onDidChangeTextDocument(event => {
+        const currentMillis = Date.now()
+        if (event.contentChanges[0].text == PREVOIUS_KEY)
+            if (currentMillis - PREVOIUS_MILLIS < DEBOUNCE_DELAY)
+                EDITOR.edit(editBuilder => {
+                    const range = new vscode.Range(
+                        event.contentChanges[0].range._start._line,
+                        event.contentChanges[0].range._start._character,
+                        event.contentChanges[0].range._end._line,
+                        event.contentChanges[0].range._end._character + 1)
+                    editBuilder.replace(range, '')
+                    ERRORS_FIXED++
+                    SESSION_ERRORS++
+                })
+        PREVOIUS_MILLIS = currentMillis
+        PREVOIUS_KEY = event.contentChanges[0].text
+    })
+
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
